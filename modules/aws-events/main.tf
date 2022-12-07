81bad9667e0283a1c4bdcf9feda4f309d5935eb8
Young-ook Kim <youngook.kim@gmail.com>
2022-12-07 20:17:07 +0900

    fix(aws-events): fix module repo

--- modules/aws-events/main.tf
+++ modules/aws-events/main.tf
@@ -1,6 +1,6 @@
 module "event" {
-  source  = "Young-ook/lambda/aws//modules/eventbridge"
-  version = "0.2.0"
+  source  = "Young-ook/eventbridge/aws"
+  version = "0.0.6"
   tags    = var.tags
   rules   = var.rules
 }
@@ -21,21 +21,12 @@ resource "aws_lambda_permission" "lambda" {
 
 # lambda
 module "lambda" {
-  source      = "Young-ook/lambda/aws"
-  version     = "0.1.4"
+  source      = "Young-ook/eventbridge/aws//modules/lambda"
+  version     = "0.0.7"
   name        = var.name
   tags        = var.tags
   lambda      = var.lambda
   tracing     = var.tracing
   vpc         = var.vpc
   policy_arns = [module.logs.policy_arns["write"]]
-}
-
-# cloudwatch logs
-module "logs" {
-  source    = "Young-ook/lambda/aws//modules/logs"
-  version   = "0.1.4"
-  name      = var.name
-  tags      = var.tags
-  log_group = var.log
 }
