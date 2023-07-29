274a806f5ac7b70eb7cc3726efb25ac457ef8e3b
Young-ook Kim <youngook.kim@gmail.com>
2023-07-29 10:45:53 +0900

    feat(iam-user): test suite

---
+++ modules/iam-user/tests/defaults/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    test = {
+      source = "terraform.io/builtin/test"
+    }
+  }
+}
+
+module "main" {
+  for_each = toset(["null", "joe"])
+  source         = "../.."
+  name           = each.key == "null" ? null : each.key
+}
+
+resource "test_assertions" "pet_name" {
+  component = "pet_name"
+  check "pet_name" {
+    description = "default random pet name"
+    condition   = can(regex("^usr", module.main["null"].user.name))
+  }
+}
+
+resource "test_assertions" "user_name" {
+  component = "pet_name"
+  check "pet_name" {
+    description = "custom user name"
+    condition   = can(regex("^joe", module.main["joe"].user.name))
+  }
+}
