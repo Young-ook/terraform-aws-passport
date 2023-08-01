ce676797ad24fe2470e1ae78991bfa8301a9813a
youngookkim <youngook.kim@gmail.com>
2023-08-01 23:56:46 +0900

    fix(iam-user): using aws-partitions module (#46)

--- modules/iam-user/main.tf
+++ modules/iam-user/main.tf
@@ -1,9 +1,8 @@
-# feature
+### feature
 locals {
   login_enabled = lookup(var.features, "login", false)
 }
 
-# iam user module
 resource "aws_iam_user" "user" {
   name          = local.name
   path          = local.namespace
@@ -11,20 +10,19 @@ resource "aws_iam_user" "user" {
   force_destroy = true
 }
 
-# security/policy
+### security/policy
 resource "aws_iam_user_policy_attachment" "policy" {
   for_each   = { for k, v in var.policy_arns : k => v }
   user       = aws_iam_user.user.name
   policy_arn = var.policy_arns[each.value]
 }
 
-# group membership
 resource "aws_iam_user_group_membership" "groups" {
   user   = aws_iam_user.user.name
   groups = var.groups
 }
 
-# security/password
+### security/password
 resource "random_password" "password" {
   length           = lookup(var.password_policy, "length", 16)
   special          = true
@@ -39,15 +37,13 @@ locals {
   }
 }
 
-data "aws_region" "current" {}
-
 resource "null_resource" "login-profile" {
   for_each = toset(local.login_enabled ? ["awscli"] : [])
   provisioner "local-exec" {
     command = "aws iam create-login-profile --cli-input-json $JSON --region $AWS_REGION"
     environment = {
       JSON       = jsonencode(local.login_profile_skaffold)
-      AWS_REGION = data.aws_region.current.name
+      AWS_REGION = module.aws.region.name
     }
   }
 }
